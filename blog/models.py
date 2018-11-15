ba180046979865aad45ff9088282321c1a35e0bf
imac <kovoli1985@gmail.com>
2018-11-15 21:28:02 +0100

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+from unidecode import unidecode
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# model managers - get the querys with Post.published.all()
+#class PublishedManager(models.Manager):
+    #def get_queryset(self):
+        #return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=250, unique_for_date='publish', unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    # objects = models.Manager()  # The default manager.
+    # published = PublishedManager()  # Our custom manager.
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(unidecode(self.title))
+        super(Post, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.slug])
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-publish', )
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
